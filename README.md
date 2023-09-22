0df87d614d75432d3198050648a7b7e25fa287c0
Jessica Ferraz <ferrazjaa@gmail.com>
2023-09-22 15:48:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ferrazjaa
+ 
---
+++ js/animation.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const helloWorld = document.getElementById("hello-world");
+    
+    // Adiciona a classe 'visible' para iniciar a animação
+    helloWorld.classList.add("visible");
+});
